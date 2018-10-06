5bf705c12b5a22d89edf45348fd3f885faf29663
galactusss <gabrielpk.18@gmail.com>
2018-10-06 09:57:07 -0300

    editing scheme, making the yarn deploy work

---
+++ subgraph/src/trustability.ts
@@ -0,0 +1,23 @@
+import { Entity, store } from '@graphprotocol/graph-ts'
+import {
+  LinkAdded,
+  LinkRemoved
+} from './types/trustability/trust'
+
+export function linkAdded(event: LinkAdded): void {
+  let link = new Entity()
+
+  link.setAddress('source', event.params.source)
+  link.setAddress('target', event.params.target)
+
+  let key = `${event.params.source}${event.params.target}`
+
+  store.set('Link', key, link)
+}
+
+export function linkRemoved(event: LinkRemoved): void {
+
+  let key = `${event.params.source}${event.params.target}`
+
+  store.set('Link', key, null)
+}
