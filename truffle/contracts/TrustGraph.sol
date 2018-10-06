a2eaad6d8e12e3db228d7d7d7778192272251b24
galactusss <gabrielpk.18@gmail.com>
2018-10-06 14:54:08 -0300

    test contract

--- truffle/contracts/TrustGraph.sol
+++ truffle/contracts/TrustGraph.sol
@@ -7,14 +7,14 @@ contract TrustGraph {
     mapping(address => mapping(address => bool)) graph;
 
     function addLink(address target) public {
-        require(msg.sender != target);
+        require(msg.sender != target, "Cannot create link to oneself");
         graph[msg.sender][target] = true;
         emit LinkAdded(msg.sender, target);
     }
 
     function removeLink(address target) public {
-        require(msg.sender != target);
-        require(graph[msg.sender][target]);
+        require(msg.sender != target, "Cannot remove link from oneself");
+        require(isLinked(msg.sender, target), "Cannot remove link that does not exist");
         graph[msg.sender][target] = false;
         emit LinkRemoved(msg.sender, target);
     }
